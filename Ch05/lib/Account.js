6c1081858947f627b76581ba1683069a26abcc3a
dbswhdns <dbswhdns@naver.com>
2022-10-10 15:44:19 +0900

    1010

--- Ch05/lib/Account.js
+++ Ch05/lib/Account.js
@@ -1,20 +1,20 @@
 class Account{
 
-    constructor (bank, id, name, balance){
+    constructor(bank, id, name, balance){
         this.bank = bank;
         this.id = id;
         this.name = name;
         this.balance = balance;
     }
 
-    deposit(money) {
+    deposit(money){
         this.balance += money;
     }
-    
+
     withdraw(money){
         this.balance -= money;
     }
-    
+
     show(){
         document.write('<p>');
         document.write('은행명 : '+this.bank+'<br/>');
@@ -23,5 +23,4 @@ class Account{
         document.write('현재잔액 : '+this.balance+'<br/>');
         document.write('</p>');
     }
-
 }
